42acca6ede4a3d2863864ca91461a922218b9d90
Gerard Cleeverea <gerard.castell@cleverea.com>
2021-08-10 10:47:17 +0200

    [ADD] config for ngx-translate-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import {
+  LocalizeRouterModule,
+  LocalizeRouterSettings,
+  LocalizeParser,
+  ManualParserLoader,
+} from '@gilsdav/ngx-translate-router';
+import { RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
+
+export function createTranslateLoader(
+  translate: TranslateService,
+  location: Location,
+  settings: LocalizeRouterSettings
+) {
+  return new ManualParserLoader(
+    translate,
+    location,
+    settings,
+    ['es', 'en'],
+    'ROUTES.'
+  );
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot(),
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'disabled',
+      relativeLinkResolution: 'legacy',
+    }),
+    LocalizeRouterModule.forRoot(routes, {
+      parser: {
+        provide: LocalizeParser,
+        useFactory: createTranslateLoader,
+        deps: [
+          TranslateService,
+          Location,
+          LocalizeRouterSettings /*, HttpClient*/,
+        ],
+      },
+      initialNavigation: true,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
